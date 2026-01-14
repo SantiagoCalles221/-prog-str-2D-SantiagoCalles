0f91818893d4cd4b459e38b89ae5688b64ab8879
SantiagoCalles221 <20253ds112@utez.edu.mx>
2026-01-13 20:15:18 -0600

    main.java

--- proyecto-01-hola-mundo/main.java
+++ proyecto-01-hola-mundo/main.java
@@ -2,5 +2,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("hola mundo");
         System.out.println("santiago calles y carranza 2D");
+        System.out.println("12 de enero de 2026")
     }
 }
