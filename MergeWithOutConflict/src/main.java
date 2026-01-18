e54205698d7f60bfe237c94d33ba1891a961ed14
Santiago <20253ds112@utez.edu.mx>
2026-01-18 13:07:26 -0600

    feat: agregar operaciones

---
+++ MergeWithOutConflict/src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args){
+        System.out.println("semana 2");
+        System.out.println("Version base");
+        System.out.println("2+2 = 4");
+        System.out.println("5*3 = 15");
+    }
+}
